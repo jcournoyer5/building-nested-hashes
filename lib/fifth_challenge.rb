c044881ef449fca5e32fad4868a39382b2db3382
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-20 20:45:26 +0000

    Done.

--- lib/fifth_challenge.rb
+++ lib/fifth_challenge.rb
@@ -2,13 +2,7 @@ require "pry"
 
 def fifth_challenge
 
-#epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
-
-#puts epic_tragedy
-
-#  =>
-epic_tragedy = {
-{
+   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Michael Jordan", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
