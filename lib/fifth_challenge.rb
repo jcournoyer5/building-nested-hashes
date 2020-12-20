b04988d7ab106cc5e5778ab51b9e7fd2f46df18c
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-20 19:11:19 +0000

    Done.

--- lib/fifth_challenge.rb
+++ lib/fifth_challenge.rb
@@ -30,9 +30,13 @@ puts epic_tragedy
 
 binding.pry
   #code your solution here:
-[:montague][:hero][:status] = "dead"
+epic_tragedy[:montague][:hero][:status] = "dead"
 
-[:capulet][:heroine][:status] = "dead"
+puts epic_tragedy
+
+epic_tragedy[:capulet][:heroine][:status] = "dead"
+
+puts epic_tragedy
 
 
   #Don't touch the following line! The method must return our newly modified epic tragedy hash
